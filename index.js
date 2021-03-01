b949db34ab5422e510054a7c92534ec05771b62f
Rami <ramiferjani31@gmail.com>
2021-03-01 17:39:16 +0100

    added static folder

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const path=require('path');
 const app=express();
-app.get('/',(req,res)=>{
-    /* res.send('<h1>Hello World</h1>'); */
+/*app.get('/',(req,res)=>{
+    /* res.send('<h1>Hello World</h1>'); 
     res.sendFile(path.join(__dirname,'public','index.html'))
-})
+})*/
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,(req,res)=>console.log(`Server Started on ${PORT}`));

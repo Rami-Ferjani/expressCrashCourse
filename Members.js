02d1c7c4a0c94e98f914bf00fe6f91c3b095b4d4
Rami <ramiferjani31@gmail.com>
2021-03-01 18:05:08 +0100

    added middleware

---
+++ Members.js
@@ -0,0 +1,21 @@
+const members=[
+    {
+        id:1,
+        name:'John Doe',
+        email:'john@gmail.com',
+        status:'active'
+    },
+    {
+        id:1,
+        name:'ahmed maraoub',
+        email:'ahmed@gmail.com',
+        status:'active'
+    },
+    {
+        id:1,
+        name:'rami ferjani',
+        email:'rami@gmail.com',
+        status:'active'
+    }
+];
+module.exports=members;
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express=require('express')
 const path=require('path');
+const { nextTick } = require('process');
 const app=express();
-/*app.get('/',(req,res)=>{
-    /* res.send('<h1>Hello World</h1>'); 
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})*/
+const members=require('./Members')
+const moment=require('./Members')
+const logger=(req,res,next)=> {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+//Init middleware
+app.use(logger);
+app.get('/api/members',(req,res)=>{
+res.json(members)
+})
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 const PORT=process.env.PORT || 5000;

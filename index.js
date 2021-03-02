9ab04d8ff4d580dc0307d18690f4fa110988d6da
Rami <ramiferjani31@gmail.com>
2021-03-02 13:56:12 +0100

    added add/modifu/update/delete members

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ const express=require('express')
 const path=require('path');
 const { nextTick } = require('process');
 const app=express();
-const members=require('./Members')
+var bodyparser=require('body-parser');
 const moment=require('moment')
 const logger=require('./middleware/logger')
 
 //set static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+//members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,(req,res)=>console.log(`Server Started on ${PORT}`));
 
 //Init middleware
-app.use(logger);
+//app.use(logger);
 
-//Gets all Members
-app.get('/api/members',(req,res)=>{
-res.json(members)
-})
-//get Single member
-app.get('/api/members/:id', (req,res)=> {
-    //res.send(req.params.id)
-    const found=members.some(member=>member.id === parseInt(req.params.id));
-    if(found){
-    res.json(members.filter(member=>member.id=== parseInt(req.params.id)))}
-    else {
-        res.status(400).json({ msg: 'Member not found'});
-    }
-})
+
+
+

a42e5440960335e64154a4602c3dd30b7b005794
Rami <ramiferjani31@gmail.com>
2021-03-16 23:28:40 +0100

    added a member

--- Members.js
+++ Members.js
@@ -1,33 +1,39 @@
-const members=[
-    {
-        id:1,
-        name:'John Doe',
-        email:'john@gmail.com',
-        status:'active'
-    },
-    {
-        id:2,
-        name:'ahmed maraoub',
-        email:'ahmed@gmail.com',
-        status:'active'
-    },
-    {
-        id:3,
-        name:'rami ferjani',
-        email:'rami@gmail.com',
-        status:'active'
-    },
-    {
-        id:3,
-        name:'rami ferjani',
-        email:'rami@gmail.com',
-        status:'active'
-    },
-    {
-        id:5,
-        name:'fares mhaya',
-        email:'fares@gmail.com',
-        status:'active'
-    }
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "ahmed maraoub",
+    email: "ahmed@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "rami ferjani",
+    email: "rami@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "rami ferjani",
+    email: "rami@gmail.com",
+    status: "active",
+  },
+  {
+    id: 5,
+    name: "fares mhaya",
+    email: "fares@gmail.com",
+    status: "active",
+  },
+  {
+    id: 5,
+    name: "ramez zormati",
+    email: "ramez@gmail.com",
+    status: "active",
+  },
 ];
-module.exports=members;
+module.exports = members;
